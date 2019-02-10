36fe488046d79ba717f29be74972a5af514085a6
JanAlbertIglesias <36413102+JanAlbertIglesias@users.noreply.github.com>
2019-02-10 22:37:10 +0100

    Simplified version of the clavsqua module.
    The quantum part is done. The classical part still needs some work.

---
+++ CCCB/clavsqua/quantumprep.py
@@ -0,0 +1,72 @@
+"""
+Jan Albert Iglesias
+06/02/2019
+
+This program pre-computes the eigenvector basis for a specific potential and writes it on a file.
+The desired directory needs to be created and specified. It'll write 3 files; one for the eigenvectors
+(altogether with the projections), the eigenvalues and the energy.
+"""
+
+import timeev as te
+import numpy as np
+
+"Definitions"
+#File:
+dir = "Demo3_qua"
+fvecs = dir + "/vecs.npy"
+fvals = dir + "/vals.npy"
+fene = dir + "/ene.npy"
+
+#Box:
+a = -20.
+b = 20.
+N = 1000
+deltax = (b - a)/float(N)
+xarr_qua = np.arange(a, b + deltax*0.1, deltax)
+
+#Physical constants:
+te.hbar = 4.136 #eV·fs (femtosecond)
+m_qua = 1/(2*3.80995) #The value contains hbar^2.
+
+#Potential:
+te.factor = 10 #Factor for the applied potential.
+height = 15
+sigma_qua = 2*te.factor/(np.sqrt(2*np.pi)*height)
+mu_qua = 0
+k_qua = 0.2
+
+#Initial wavefunction:
+te.sigma0 = 0.4
+te.p0 = 0
+xo = -1.2
+
+#Method definitions:
+Nbasis = 200
+coefs = np.zeros(shape = (Nbasis + 1, 1), dtype=complex)
+coef_x_efuns = np.zeros(shape = (N + 1, Nbasis + 1), dtype=complex)
+evalsbasis = np.zeros(shape = (Nbasis + 1, 1))
+psivec = te.psi(xo, xarr_qua)
+
+"Solving Schrödinger equation"
+#Computes the eigenvalues and eigenvectors.
+evals, efuns = te.srindwall(a, b, N, m_qua, te.pot, mu_qua, sigma_qua, k_qua)
+
+#Computes the projection of the given wavefunction onto the wavefunction basis.
+for j in range(Nbasis + 1):
+    prod = np.conjugate(psivec)*efuns[:,j]                        #e*(x)·psi(x)
+    coefs[j] = deltax*(np.sum(prod) - prod[0]/2. - prod[-1]/2.)    #Trapezoidal rule for integration.
+    coef_x_efuns[:,j] = coefs[j]*efuns[:,j]
+    evalsbasis[j,0] = evals[j]                                       #It only uses Nbasis values.
+
+#Computes the energy.
+energy = np.sum(np.abs(coefs)**2*evalsbasis)
+
+"Saving on a file"
+np.save(fvecs, coef_x_efuns)
+np.save(fvals, evalsbasis)
+np.save(fene, energy)
+
+"Reading the file" #Not needed, just to see how it's readed.
+r_coef_x_efuns = np.load(fvecs)
+r_evalsbasis = np.load(fvals)
+r_energy = np.load(fene)
