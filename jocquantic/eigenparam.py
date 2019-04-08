eac0baa2d496b914ca66d74396e087f36a1680f1
CanalsManu <48415022+CanalsManu@users.noreply.github.com>
2019-04-08 13:49:04 +0200

    Code checking

--- jocquantic/eigenparam.py
+++ jocquantic/eigenparam.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 """
 Manu Canals Codina
-
-Eigenvalues and eigenfunctions of a time-independent potential (Hamiltonian).
 """
 #%%
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-#Potential.
+#Potential
+#For using these in eigemparam they need to be in units eV
 def gaussian(mu, sigma, x):
     f = 1./np.sqrt(2*np.pi*sigma**2)*np.exp(-(x-mu)**2/(2.*sigma**2))
     return f
@@ -20,17 +19,24 @@ def harmonic(k, x):
     return V
 
 def poten(x):
-    pot = 10*2*(0.4*gaussian(0, 0.6, x) + harmonic(0.1, x))  #Potential
-    #Factor 10 introduced to minimize the influence of the walls, since a 
+    pot = (20*gaussian(0, 2, x) + harmonic(0.1, x))  #Potential
+    #factor 10 introduced to minimize the influence of the walls, since a 
     #larger potential keeps the main structure of the wave function centered.
     return pot
 
+#Hamiltonian's eigenvalues and vectors.
 def eigenparam(a, b, N, m, poten):
     """
-    Returns the a vector with the eigenvalues and another with the eigenvectors
-    (each eigenvector of dimention N). It solves the time-independent 
-    Schrödinger equation for a given potentitial [poten] inside of a 
-    box [a, b], with [N] intervals. [m] := hbar**2 / M  (M = mass) 
+    Returns a vector with the eigenvalues(eV) and another with the eigenvectors
+    ((Aº·fs)**-1/2) of the quantum hamiltonian with potential [poten(eV)] (each
+    columns is an eigenvector with [N] components). 
+    It solves the 1D time-independent Schrödinger equation for the given 
+    
+        H · phi = E · phi  ;  H = -(1/2m)(d**2/dx**2) + poten(x)
+                m := mass / hbar**2  [(eV·Aº**2)**-1]
+    
+    potential (callable poten(x[eV])) inside of a box [a(Aº), b(Aº)], with [N] 
+    intervals. 
     """
     deltax = (b-a)/float(N)
     
@@ -41,7 +47,7 @@ def eigenparam(a, b, N, m, poten):
     
     H = np.zeros((N+1,N+1))
     
-    H[0,0] = 1./(m*deltax**2) + 1000000000
+    H[0,0] = 1./(m*deltax**2) + 1000000000#(eV)
     H[N,N] = 1./(m*deltax**2) + 1000000000
     H[1,0] = -1./(2.*m*deltax**2)
     H[N-1,N] = -1./(2.*m*deltax**2)
@@ -55,21 +61,11 @@ def eigenparam(a, b, N, m, poten):
     #in columns).
     evals, evect = np.linalg.eigh(H)
     
-    #Normalization
+    #Normalization. Used trapezoid's method formula, given that sum(evect**2)=1
     factor = np.zeros((N+1)) #Evect will be multiplied by 1/sqrt(factor)
-    #Integrating with trapezoids
     for col in range(N+1):
-        for line in range(N):
-            #The area of one trapezoid is added every iteration
-            factor[col] += ( (np.abs(evect[line,col]))**2 + 
-                          (np.abs(evect[line+1,col]))**2 )*deltax / 2.
-                  
-    #Check if the sum of all components of any evect gives 1
-    check = 0
-    for elem in evect[:,40]:
-        check += elem**2    
-    #print(check)
-    
+        factor[col] = deltax * (1. - evect[0,col]/2. - evect[-1,col]/2.)
+
     #Normalized vectors
     for col in range(N+1):
         evect[:,col] *= 1/np.sqrt(factor[col])
@@ -85,122 +81,125 @@ def norm(vector, dx):
     return n
 
 #%%
-#Check normalization and first run of eigenparam (results used later)
-a = -5.
-b= 5.
+#First run of eigenparam (results used later)
+a = -10.  #Aº
+b= 10.    #Aº
+m = 0.13 #For an electron (eV·Aº**2)**-1/2
 N = 100
-m = 1
 dx = (b-a)/float(N)
+#Mesh for all the plots (includes the boundaries)
+mesh = np.linspace(a, b, N+1)
 
 pvals, pvect = eigenparam(a, b, N, m, poten)
-
-normm = np.zeros((N+1))
-
-for col in range(N+1):
-    for line in range(N):
-        #Area of one trapezoid is added every iteration
-        normm[col] += ( (np.abs(pvect[line,col]))**2 + 
-                      (np.abs(pvect[line+1,col]))**2 )*dx / 2.
-
-#print(normm[7])            
-#print(norm((np.abs(pvect[:,7]))**2, dx))
-            
-#x = np.linspace(a, b, N+1)
-#plt.title('Factor del potencial')
-#plt.plot(x, 0.1*poten(x))
-#plt.plot(x, poten(x))
-#%%
-
-#plt.plot(pvect[:,3])
-#plt.plot(pvect[:,450])
-#plt.plot(pvect[:,100])
-#plt.plot(pvect[:,250])
-
-#plt.yscale('log')
-#plt.plot(pvals)
-
 #%%
 
 #Projection of the given intial wave function onto the basis {evect}
 #Initial wave function
 
-def psi0(points):
+def gpacket(points, sigma0):
     """
     Generates a vector with the values of the initial wave function in the 
-    given points (psi0 dimension same as points).
+    given points(Aº) (psi0 dimension same as points), hera a gaussian packet.
     Wave function introduced here explicitly.
     
     """
-    #In this case psi0 is a gaussian packet (as an example)
+    #In this case psi0 is a gaussian packet 
     mu = points[int(len(points)*0.4)] 
-    sigma0 = 0.5
-    p0 = 0.5 #momentum
+    #sigma0 = 0.8
+    p0 = 0 #momentum
+    
     wavef = np.sqrt((1./(np.sqrt(2*np.pi)*sigma0))*np.exp(-(points-mu)**2/
                      (2.*sigma0**2)))*np.exp(complex(0,-1)*p0*points)
     return wavef
 
+def psi0(points):
+    """
+    Generates a vector with the values of the initial wave function in the 
+    given points(Aº) (psi0 dimension same as points).
+    Wave function introduced here explicitly.
+    
+    """
+    #TRIANGLE WF
+    waveflist = [p + 5 if p <= 0 else -p + 5 for p in points]
+    wavef = np.array(waveflist)
+
+    return wavef/25.
+
 def comp(evect, psi, deltax):
     """
-    Given a basis evect and a vector psi, returns psi's components.
+    Given a basis evect and a vector psi, returns a vector with psi's 
+    components.
     """
     compo = []
-    for ev in np.transpose(evect):
+    for ev in np.transpose(evect): #ev: each basis vector
+        #for each ev, we integrate ev conjugate * psi = integrand 
         integrand =[np.conjugate(v)*p for v, p in zip(ev, psi)] 
+        #integrations by trapezoids
         compo.append(deltax*(sum(integrand)-integrand[0]/2.-integrand[-1]/.2))
         
-    return compo
+    return np.array(compo)
+#%%
+sigma = []
+angleE = []
+for s in np.arange(0.1, 1.5, 0.2):
+    psi = gpacket(mesh,s)
+    psicomp = comp(pvect, psi, (b-a)/float(N))
+    expect_E = sum([p*np.abs(c)**2 for p,c in zip(pvals, psicomp)])
+    sigma.append(s)
+    angleE.append(expect_E) 
     
-mesh = np.arange(a, b + (b-a)/float(N), ((b-a)/float(N)))
-psi = psi0(mesh)
+plt.title(r'$\left\langle E \right\rangle \ vs. \sigma $')
+plt.yscale('log')
+plt.ylabel(r'$\left\langle E \right\rangle \  (eV)$' )
+plt.xlabel(r'$\sigma \  (\AA)$')
+plt.plot(sigma, angleE) 
 
-#print(norm((np.abs(psi))**2, dx))
+  
 
-#plt.plot(psi)
-
-psicomp = comp(pvect,psi,(b-a)/float(N))
-#print(proj, pvals)
-#plt.title('Components de psi0')
-#plt.xlim(0,30)
-#plt.plot(psicomp)
-
+#print(expect_E)
+#plt.plot(mesh, [poten(point) for point in mesh])
+#plt.plot(mesh, [expect_E for i in mesh])
 #%%
 #Animation (using all the components)
 # First set up the figure, the axis, and the plot element we want to animate
 fig = plt.figure()
 ax = plt.axes(xlim=(a, b), ylim=(0, 1))
-line, = ax.plot([], [], lw=2)
-pot, = ax.plot([], [], lw = 1)
+line, = ax.plot([], [], lw=2, label = '$\Psi(x,t)$')
+pot, = ax.plot([], [], lw = 1, label = '$V(x)$')
 
-# initialization function (of each frame): plot the background
+x = np.linspace(a, b, N+1)
+# initialization function (of each frame): plot the background 
 def init():
-    line.set_data([], [])
-    x = np.linspace(a, b, N+1)
-    y = poten(x)/10.#It's the factor explined in poten, we rescale the 
-                    #potential here so it fits in the plot.
+    line.set_data([], []) #reset each frame
+    y = poten(x)/10. #Factor so the potential fits the plot
+    plt.title('$|\Psi(x,t)|^2$', fontweight= 'bold', fontsize = 20, pad = 10)
+    plt.xlabel('$x (\AA)$', fontsize=15)
+    plt.ylabel('$|\Psi(x,t)|^2 \ \ (\AA^{-1/2}·fs^{-1/2})$', fontsize=15)
+    plt.legend()
     pot.set_data(x, y)
     return line,
 
 # animation function.  This is called sequentially
 def fullanimate(t):  
-    x = np.linspace(a, b, N+1)
-    y = []
+    wf = []
     for i in range(0, N+1): #each component
         acum = 0
         for j in range(0, N+1): #each eigenvector
             acum += psicomp[j]*pvect[i,j]*np.exp(complex(0,-1)*pvals[j]*t/5)
-        y.append((np.abs(acum))**2)
+        wf.append((np.abs(acum))**2) #We add the probability
     
-    line.set_data(x, y)
+    line.set_data(x, wf)
     
     return line,
+
 
 # call the animator.  blit=True means only re-draw the parts that have changed.
 anim = animation.FuncAnimation(fig, fullanimate, init_func=init, frames=100, 
                                                        interval=100, blit=True)
-
 anim.save('psievo.gif')
 
 plt.show()
+
 
 #%%
 
@@ -222,6 +221,30 @@ plt.title('Diferència(abs) de la norma entre psi truncada i sencera')
 plt.yscale('log')
 plt.plot(diff)
 #Here we see that we hit machine presicion with lv around 40.
+
+
+
+
+
+#Check pvals (energy values) with its evect component 
+#fig, ax1 = plt.subplots()
+#
+#ax1.plot(pvals, 'b')
+#
+#ax1.set_yscale('log')
+#ax1.set_xlabel('index')
+#ax1.set_ylabel('pvals', color='b')
+#
+#ax2 = ax1.twinx()
+#ax2.plot(psicomp, 'r')
+#
+#ax2.set_ylabel('comp', color = 'r')
+#
+#fig.tight_layout()
+#plt.show()
+
+
+
 #%%
 
 
