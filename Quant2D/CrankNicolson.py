e27a09cd50f678a825ea9de9e6de15003ebc90d2
marinaorta <48490279+marinaorta@users.noreply.github.com>
2019-05-04 14:28:26 +0200

    Add files via upload

--- Quant2D/CrankNicolson.py
+++ Quant2D/CrankNicolson.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as ss
+from matplotlib import animation
 
 #TRIDIAGONAL ALGORITHM
 def tridiag(a, b, c, d):
@@ -32,82 +33,91 @@ def tridiag(a, b, c, d):
 
 #CRANK-NICOLSON
 def CrankNicolson (dx,dt,ndim,ntime,r,V,psi):
-    #A sup and inf diagonals
-    Asup=np.full(ndim,-r,dtype=complex)
-    Ainf=np.full(ndim,-r,dtype=complex)
     
-    Asup[ndim-1]=0.
-    Ainf[0]=0.
+    #A sup and inf diagonals
+    A1=np.full(ndim,-r,dtype=complex)
+    A3=np.full(ndim,-r,dtype=complex)
+    
+    A1[ndim-1]=0.
+    A3[0]=0.
     
     #B matrix
-    Bmatrix=np.zeros((ndim,ndim),dtype=complex)
+    B=np.zeros((ndim,ndim),dtype=complex)
     for i in range(0,ndim):
         for j in range(0,ndim):
             if i==j:
                 x=i*dx
                 y=j*dx
-                Bmatrix[i,j]=1.-2.*r-1j*dt*V(x,y)/(4.*hbar)
+                B[i,j]=1.-2.*r-1j*dt*V(x,y)/(4.*hbar)
             if i==j+1 or i+1==j:
-                Bmatrix[i,j]=r
+                B[i,j]=r
     
-    norm=np.array([])
-    "Time evolution"
+    norma=[]
     for t in range(0,ntime+1):
         
-        "Evolve for x"
-        for j in range(0,ndim):
-            
-            #Adiag
-            Adiag=np.array([])
-            for i in range(0,ndim):
-                x=i*dx
-                y=j*dx
-                Adiag=np.append(Adiag,(1.+2.*r+1j*dt*V(x,y)/(4.*hbar)))
-            #Psix
-            psix=np.array([])
-            for i in range(0,ndim):
-                psix=np.append(psix,psi[i,j])
-            #Bmatrix*Psi0 
-            Bproduct=np.dot(Bmatrix,psix)
-            
-            #Tridiagonal
-            psix=tridiag(Ainf,Adiag,Asup,Bproduct)
-            
-            #Change the old for the new values of psi
-            for i in range(0,ndim):
-                psi[i,j]=psix[i]
-                
-        "Evolve for y"
-        for i in range(0,ndim):
-            
-            #Adiag
-            Adiag=np.array([])
-            for j in range(0,ndim):
-                x=i*dx
-                y=j*dx
-                Adiag=np.append(Adiag,(1.+2.*r+1j*dt*V(x,y)/(4.*hbar)))
-            #Psix
-            psiy=np.array([])
-            for j in range(0,ndim):
-                psiy=np.append(psiy,psi[i,j])
-            #Bmatrix*Psi 
-            Bproduct=np.dot(Bmatrix,psiy)
-            #Tridiagonal
-            psiy=tridiag(Ainf,Adiag,Asup,Bproduct)
-            
-            #Change the old for the new values of psi
-            for j in range(0,ndim):
-                psi[i,j]=psiy[j]
-                
+        psi=CrankStep(dx,dt,ndim,r,V,psi,A1,A3,B)
+        
         prob=Probability(psi,ndim)
-        norm=np.append(norm,Norm(prob,ndim,dx))
+        print(Norm(prob,ndim,dx), t)
+        norma=np.append(norma,Norm(prob,ndim,dx))
     
-    return psi,norm
+    return psi,norma
+
+def CrankStep (dx,dt,ndim,r,V,psi,Asup,Ainf,Bmatrix):    
+    "1 timestep evolution"
+            
+    "Evolve for x"
+    for j in range(0,ndim):
+        
+        #Adiag
+        Adiag=[]
+        for i in range(0,ndim):
+            x=i*dx
+            y=j*dx
+            Adiag=np.append(Adiag,(1.+2.*r+1j*dt*V(x,y)/(4.*hbar)))
+        #Psix
+        psix=[]
+        for i in range(0,ndim):
+            psix=np.append(psix,psi[i,j])
+        #Bmatrix*Psi0 
+        Bproduct=np.dot(Bmatrix,psix)
+        
+        #Tridiagonal
+        psix=tridiag(Ainf,Adiag,Asup,Bproduct)
+        
+        #Change the old for the new values of psi
+        for i in range(0,ndim):
+            psi[i,j]=psix[i]
+            
+    "Evolve for y"
+    for i in range(0,ndim):
+        
+        #Adiag
+        Adiag=[]
+        for j in range(0,ndim):
+            x=i*dx
+            y=j*dx
+            Adiag=np.append(Adiag,(1.+2.*r+1j*dt*V(x,y)/(4.*hbar)))
+        #Psix
+        psiy=[]
+        for j in range(0,ndim):
+            psiy=np.append(psiy,psi[i,j])
+        #Bmatrix*Psi 
+        Bproduct=np.dot(Bmatrix,psiy)
+        #Tridiagonal
+        psiy=tridiag(Ainf,Adiag,Asup,Bproduct)
+        
+        #Change the old for the new values of psi
+        for j in range(0,ndim):
+            psi[i,j]=psiy[j]
+            
+    return psi
 
 #POTENTIAL
 def Pot(x,y):
     k=1.
-    return 0.5*k*((x-2.5)**2+(y-2.5)**2)
+    x0=ndim*dx/2.
+    return 0.5*k*((x-x0)**2+(y-x0)**2)
 
 #INITIAL FUNCTION
         #x->a
@@ -122,6 +132,21 @@ def EigenOsci(x,y,a,b):
     Hy=ss.eval_hermite(b,zety)
     c_ab=(2**(a+b)*np.math.factorial(a)*np.math.factorial(b)*np.pi)**(-0.5)
     return c_ab*np.exp(-0.5*(zetx**2+zety**2))*Hx*Hy
+
+def Coherent(x,y,x0,y0):
+    k=1.
+    m=1.
+    w=np.sqrt(k/m)
+    
+#    a=b=0.
+    xx=x-x0
+    yy=y-y0
+    
+    c=(m*w/(np.pi*hbar))**0.5
+    e1=1.
+    e2=1.  #np.exp(np.sqrt(2*m*w/hbar)*(a*xx+b*yy))
+    e3=np.exp(-0.5*(m*w/hbar)*(xx**2+yy**2))
+    return c**2*e1*e2*e3
 
 #PROBABILITY
 def Probability(f,n):
@@ -150,96 +175,115 @@ def Norm(probab,dim,pas):
     return norm*pas**2
     
 
-ndim=n=100
 
-ntime=100
+
+#FIGURE SHOWING POTENTIAL, NORM EVOLUTION AND INTIAL+FINAL PROBABILITY
+def fig(Fu0,V,ndim,ntime,a,b):
+        #Potential matrix
+    po=Potential_matrix(V,ndim)
+        
+        #Initial psi
+    psi0=np.zeros((ndim,ndim),dtype=complex)
+    for i in range (0,ndim):
+        for j in range (0,ndim):
+            x=dx*i
+            y=dx*j
+            psi0[i,j]=Fu0(x,y,a,b)
+    Ppsi0=Probability(psi0,ndim)
+    
+        #Final psi
+    psi,norma=CrankNicolson(dx,dt,ndim,ntime,r,V,psi0)
+    Ppsi=Probability(psi,ndim)
+    
+        #Print norms        
+    print('Norm ini t')
+    print(Norm(Ppsi0,ndim,dx))
+    print('Norm final t')
+    print(Norm(Ppsi,ndim,dx))
+    
+        #Figures
+    plt.figure()
+            #Potential (colormap)
+    plt.subplot(2,2,1)
+    plt.title('Potential')
+    plt.imshow(po,cmap="plasma")
+            #Initial psi
+    plt.subplot(2,2,2)
+    plt.title('Initial')
+    plt.imshow(Ppsi0,cmap="plasma")
+    plt.axis('off')
+            #Final psi
+    plt.subplot(2,2,4)
+    plt.title('Final')
+    plt.imshow(Ppsi,cmap="plasma")
+    plt.axis('off')
+            #Norm evolution
+    plt.subplot(2,2,3)
+    plt.title('Evol norma')
+    plt.plot(norma)
+    
+    plt.show()
+    return
+
+
+#GIF OF TIME EVOLUTION
+def anim(Fu0,V,ndim,ntime,a,b):
+    
+    fig = plt.figure()
+    
+    #Initial Psi
+    psi=np.zeros((ndim,ndim),dtype=complex)
+    for i in range (0,ndim):
+        for j in range (0,ndim):
+            x=dx*i
+            y=dx*j
+            psi[i,j]=Fu0(x,y,a,b)
+    print(Norm(Probability(psi,ndim),ndim,dx))
+    #A sup and inf diagonals
+    A1=np.full(ndim,-r,dtype=complex)
+    A3=np.full(ndim,-r,dtype=complex)
+    
+    A1[ndim-1]=0.
+    A3[0]=0.
+    
+    #B matrix
+    B=np.zeros((ndim,ndim),dtype=complex)
+    for i in range(0,ndim):
+        for j in range(0,ndim):
+            if i==j:
+                x=i*dx
+                y=j*dx
+                B[i,j]=1.-2.*r-1j*dt*V(x,y)/(4.*hbar)
+            if i==j+1 or i+1==j:
+                B[i,j]=r
+    #Animation
+    ims = []
+    for i in range(ntime):
+        psi=CrankStep(dx,dt,ndim,r,V,psi,A1,A3,B)
+        im = plt.imshow(Probability(psi,ndim), animated=True)
+        ims.append([im])
+    
+    ani=animation.ArtistAnimation(fig,ims,interval=50,blit=True,repeat_delay=500)
+    print(Norm(Probability(psi,ndim),ndim,dx))
+#    ani.save('TEvol.gif')
+    ani.show()
+    return
+    
+#############################################################################
+
+ndim=n=100
+#ntime=100
 
 dx=0.05
 dt=0.01
 
 m=1.
 hbar=1.
-
 r=1j*dt/(4.*m*dx**2)
 
-#Potential matrix
-po=Potential_matrix(Pot,ndim)
 
+f=fig(EigenOsci,Pot,ndim=100,ntime=100,a=0,b=0)
+#an=anim(EigenOsci,Pot,ndim,ntime=100,a=0,b=0)
 
-"Estat fonamental: (0,0)"
-
-#Initial psi
-psi0=np.zeros((ndim,ndim),dtype=complex)
-for i in range (0,ndim):
-    for j in range (0,ndim):
-        x=dx*i
-        y=dx*j
-        psi0[i,j]=EigenOsci(x,y,0,0)
-Ppsi0=Probability(psi0,ndim)
-
-#Final psi
-psi,norma1=CrankNicolson(dx,dt,ndim,ntime,r,Pot,psi0)
-Ppsi=Probability(psi,ndim)
-
-
-#Figures
-print(Norm(Ppsi0,ndim,dx))
-print(Norm(Ppsi,ndim,dx))
-
-plt.figure()
-
-#plt.subplot(3,3,1)
-#plt.title('Potential')
-#plt.imshow(po,cmap="plasma")
-
-plt.subplot(2,3,2)
-plt.title('Initial')
-plt.imshow(Ppsi0,cmap="plasma")
-plt.axis('off')
-
-plt.subplot(2,3,3)
-plt.title('Final')
-plt.imshow(Ppsi,cmap="plasma")
-plt.axis('off')
-
-"Estat (0,1)"
-
-#Initial psi
-psi0=np.zeros((ndim,ndim),dtype=complex)
-for i in range (0,ndim):
-    for j in range (0,ndim):
-        x=dx*i
-        y=dx*j
-        psi0[i,j]=EigenOsci(x,y,0,1)
-Ppsi0=Probability(psi0,ndim)
-
-#Final psi
-psi,norma2=CrankNicolson(dx,dt,ndim,ntime,r,Pot,psi0)
-Ppsi=Probability(psi,ndim)
-
-print(Norm(Ppsi0,ndim,dx))
-print(Norm(Ppsi,ndim,dx))
-
-#plt.subplot(2,3,4)
-#plt.title('Potential')
-#plt.imshow(po,cmap="plasma")
-
-plt.subplot(2,3,5)
-plt.title('Initial')
-plt.imshow(Ppsi0,cmap="plasma")
-plt.axis('off')
-
-plt.subplot(2,3,6)
-plt.title('Final')
-plt.imshow(Ppsi,cmap="plasma")
-plt.axis('off')
-
-
-    #Evol norma
-
-plt.subplot(2,3,1)
-plt.title('Evol norma')
-plt.plot(norma1)
-plt.subplot(2,3,4)
-plt.title('Evol norma')
-plt.plot(norma2)
+#f=fig(Coherent,Pot,ndim=100,ntime=100,a=1.,b=2.5)
+#an=anim(Coherent,Pot,ndim,ntime=200,a=2.,b=2.5)
