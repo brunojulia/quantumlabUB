a7ab6163d9e1928aa0bbedf8c480ca475a08b68a
marinaristol <72804649+marinaristol@users.noreply.github.com>
2021-04-29 12:56:32 +0200

    Add files via upload

--- Encriptació/network.py
+++ Encriptació/network.py
@@ -13,7 +13,8 @@ import pickle
 class Network:
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server= "192.168.0.28"
+        #self.server= "192.168.0.35"
+        self.server=input("Per continuar, introdueix la IP del server : ")
         self.port= 5555
         self.addr = (self.server, self.port)
         self.p = self.connect() #ha posat self.pos
