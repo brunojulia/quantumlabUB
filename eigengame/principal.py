3060844247dec8bc0cc2a955365acaa4134a4e37
Francesc Sabater <101269096+francescsabater@users.noreply.github.com>
2022-03-20 19:28:02 +0100

    Add files via upload

--- eigengame/principal.py
+++ eigengame/principal.py
@@ -3,15 +3,18 @@ from kivy.app import App
 from kivy.lang import Builder 
 from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.uix.floatlayout import FloatLayout 
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 from kivy.properties import ObjectProperty
+from kivy.animation import Animation 
+from kivy.uix.image import Image
 import matplotlib.pyplot as plt 
 import math as math
 import numpy as np  
 import scipy.special 
-import decimal 
-from decimal import Decimal
+import random 
+
 
 
 
@@ -21,6 +24,7 @@ class MainWindow(Screen):
      
 class TrainerWindow(Screen):
         float_plot=ObjectProperty(None)
+        measure_layout=ObjectProperty(None)
         value_n=0#if no button is pressed
         hooke_constant=0 #if it isn't slide it
         x0_harmonic=0 #if there is no sliding
@@ -97,7 +101,6 @@ class TrainerWindow(Screen):
                 self.button_free.background_color=(1,1,1,1)
                 self.button_barrier.background_color=(1,1,1,1)
                 self.button_water.background_color=(1,1,1,1)
-
         def slider_value_k(self,*args):
                 ''' Takes the hooke constant value from a slider''' 
                 self.hooke_constant=args[1]
@@ -132,7 +135,6 @@ class TrainerWindow(Screen):
                 self.Vb=args[1]
                 self.barrier()
                 self.plot_potential()
-
         def barrier(self): 
                 self.potential_type="Barrier"
                 self.button_harmonic.background_color=(1,1,1,1)
@@ -156,14 +158,13 @@ class TrainerWindow(Screen):
                 self.water_well()
                 self.plot_potential()
         def water_well(self): 
-                self.potential_type="Water_well"
+                self.potential_type="well"
                 self.button_harmonic.background_color=(1,1,1,1)
                 self.button_free.background_color=(1,1,1,1)
                 self.button_barrier.background_color=(1,1,1,1)
                 self.button_water.background_color=(1,0,0,1)
 
 
-        
         def potential(self,x): 
                 '''Gives the potential'''
                 if self.potential_type=="Harmonic":
@@ -178,7 +179,7 @@ class TrainerWindow(Screen):
                                 if x<self.left_x_b: V=0 
                                 if x>self.left_x_b and x <self.right_x_b: V=self.Vb  
                                 if x>self.right_x_b: V=0  
-                if self.potential_type=="Water_well":
+                if self.potential_type=="well":
                         V=0 #if the condition is not satisfied V=0
                         #we check that the left position is smaller than the right one 
                         if self.left_x_w<self.right_x_w: 
@@ -252,11 +253,12 @@ class TrainerWindow(Screen):
         def plot_energy(self):
                 '''This function plots the energy line for a estatic potential'''
                 plt.clf()
+                self.measure_layout.clear_widgets()
                 fig, ax_phi=plt.subplots()
                 ax_phi.set_xlabel(r"$x(\AA)$")
                 ax_phi.set_ylabel(r"$ \phi^2$")
                 ax_phi.set_ylim((0,1)) #maximum of phi_axis
-                
+                ax_phi.xaxis.labelpad = -1
                 #we compute the eigen_values 
                 A=self.matrix()
                 eigenval,eigenvec=np.linalg.eig(A) #we compute eigenvalues and eigenvectors 
@@ -280,6 +282,7 @@ class TrainerWindow(Screen):
                 plt.title("E"+str(self.value_n)+"(eV)="+str(E)[0:4]+"   V(x)= "+str(self.potential_type),loc="right")
                 
                 canvas_plot=FigureCanvasKivyAgg(plt.gcf())
+                self.float_plot.clear_widgets()
                 self.float_plot.add_widget(canvas_plot)
 
         def plot_potential(self): 
@@ -288,12 +291,14 @@ class TrainerWindow(Screen):
                 every time the potential is changing'''
                 #clear previous plot 
                 plt.clf()
+                self.measure_layout.clear_widgets()
 
 
                 fig, ax_phi=plt.subplots()
                 ax_phi.set_xlabel(r"$x(\AA)$")
                 ax_phi.set_ylabel(r"$ \phi^2$")
                 ax_phi.set_ylim((0,1)) #maximum of phi_axis
+                ax_phi.xaxis.labelpad = -1
 
                 y2=0
                 V_plot=[self.potential(s) for s in self.x_list_values] 
@@ -306,6 +311,7 @@ class TrainerWindow(Screen):
                 plt.title("V(x)= "+str(self.potential_type),loc="right")
                 
                 canvas_plot=FigureCanvasKivyAgg(plt.gcf())
+                self.float_plot.clear_widgets()
                 self.float_plot.add_widget(canvas_plot)
 
         def plot_wave_function(self):
@@ -314,7 +320,7 @@ class TrainerWindow(Screen):
                 and computes the eigenvector. '''
                 #clear previous plot 
                 plt.clf()
-                
+                self.measure_layout.clear_widgets()
                 phi_square, E=self.wave_function()#we compute the eigen_values  and assign E 
                  
                 y2=0
@@ -323,10 +329,11 @@ class TrainerWindow(Screen):
                 fig, ax_phi=plt.subplots() 
                 ax_phi.set_xlabel(r"$x(\AA)$")
                 ax_phi.set_ylabel(r"$ \phi^2$")
+                ax_phi.xaxis.labelpad = -1
 
                 #CHECKING
                 #we check free partcile with theory to see if we are right 
-                x_4= np.linspace(-5,5,401)
+                #x_4= np.linspace(-5,5,401)
                 #y_4=((1/math.sqrt(5))*np.sin(4*math.pi*x_4/10))**2 #plot E4
                 #y_4=((1/math.sqrt(5))*np.cos(5*math.pi*x_4/10))**2 #plot E5
                 #ax_phi.plot(x_4,y_4,label= "Analytic", color='blue')
@@ -378,7 +385,95 @@ class TrainerWindow(Screen):
                 plt.title("E"+str(self.value_n)+"(eV)="+str(E)[0:4]+"   V(x)= "+str(self.potential_type),loc="right")
                 
                 canvas_plot=FigureCanvasKivyAgg(plt.gcf())
+                self.float_plot.clear_widgets()
                 self.float_plot.add_widget(canvas_plot)
+                
+        def measure(self): 
+                '''This function measures the position of the electron and plots the electron into the screen
+                Calls another function to plot the electron when the animations is complete'''
+                self.measure_layout.clear_widgets()  #we clear any electron plotted before
+                #we make n_rays 
+                n_rays=1
+                thunder_grid=[None]*n_rays #empty list of 4 elements 
+                thunder_anim=[None]*n_rays
+                thunder_graphs=[None]*n_rays
+                y_ray=0
+                for ray in range(0,n_rays):
+                        y_ray+=1/(n_rays+1)
+                        thunder_grid[ray]=GridLayout(rows=1,cols=1) #we create a gridlayout to put the image 
+                        thunder_grid[ray].pos_hint={"center_x":0.5,"center_y":y_ray} #we put the gridlayout in the middle
+                        thunder_grid[ray].size_hint_x=0
+                        thunder_grid[ray].size_hint_y=0
+                        thunder_grid[ray].add_widget(
+                                Image(source="graphs/thunder.PNG",allow_stretch=True,keep_ratio=False)) #we import image       
+                        thunder_anim[ray]= Animation(size_hint_x=1,size_hint_y=1,duration=0.005) #we make appear the ray
+                        thunder_anim[ray]+=Animation(size_hint_x=0,size_hint_y=0,duration=0.005) #we make disappear the ray
+                        thunder_anim[ray].start(thunder_grid[ray])
+                        self.measure_layout.add_widget(thunder_grid[ray]) #we add the thundergrid layout to the float layout
+                
+                thunder_anim[n_rays-1].bind(on_complete=self.appear_e) #when the animation is complete we call appear electron
+        
+        def multi_measure(self): 
+                '''This function measures the position of the electron and plots the electron into the screen
+                Calls another function to plot the electron when the animations is complete'''
+                self.measure_layout.clear_widgets()  #we clear any electron plotted before
+                #we make n_rays 
+                n_rays=1
+                thunder_grid=[None]*n_rays #empty list of 4 elements 
+                thunder_anim=[None]*n_rays
+                thunder_graphs=[None]*n_rays
+                y_ray=0
+                for ray in range(0,n_rays):
+                        y_ray+=1/(n_rays+1)
+                        thunder_grid[ray]=GridLayout(rows=1,cols=1) #we create a gridlayout to put the image 
+                        thunder_grid[ray].pos_hint={"center_x":0.5,"center_y":y_ray} #we put the gridlayout in the middle
+                        thunder_grid[ray].size_hint_x=0
+                        thunder_grid[ray].size_hint_y=0
+                        thunder_grid[ray].add_widget(
+                                Image(source="graphs/thunder.PNG",allow_stretch=True,keep_ratio=False)) #we import image       
+                        thunder_anim[ray]= Animation(size_hint_x=1,size_hint_y=1,duration=0.005) #we make appear the ray
+                        thunder_anim[ray]+=Animation(size_hint_x=0,size_hint_y=0,duration=0.005) #we make disappear the ray
+                        thunder_anim[ray].start(thunder_grid[ray])
+                        self.measure_layout.add_widget(thunder_grid[ray]) #we add the thundergrid layout to the float layout
+                
+                thunder_anim[n_rays-1].bind(on_complete=self.appear_multi_e) #when the animation is complete 
+
+        
+
+        def appear_e(self,*args): 
+                '''Plots the elctron'''
+                probabilities,E=self.wave_function() #compute the probabilities and energy 
+                position=random.choices(self.x_list_values,weights=probabilities,k=1) #computes the position accordingly to WF
+                e_position=(position[0]+5)/10 #normalising to 0-1 in the box layout
+                e_graphs=Image(source="graphs/electron.PNG",allow_stretch=True,keep_ratio=False) #we import image
+                #clear image before 
+                e_grid=GridLayout(rows=1,cols=1) #we create a gridlayout to put the image 
+                e_grid.pos_hint={"center_x":e_position,"center_y":0.5} #we put the gridlayout in the position we want
+                e_grid.size_hint_x=0.06
+                e_grid.size_hint_y=0.25 #size of gridlayout
+                e_grid.add_widget(e_graphs) #add the image to the gridLayout
+                self.measure_layout.add_widget(e_grid) #we add the electron grid layout to the float layout
+
+        def appear_multi_e(self,*args): 
+                '''This function measures the position of the n electrons and plots the electrons into the screen. 
+                acts like self.appear_e but with n_e electrons'''
+                n_e=200
+                probabilities,E=self.wave_function() #compute the probabilities and energy 
+                position=random.choices(self.x_list_values,weights=probabilities,k=n_e) #computes the position accordingly to WF
+                #we create a lists of the gridslayouts with are working with 
+                gridlayout=[None]*n_e #list of 100 elements
+                for i in range(0,n_e): 
+                        e_graphs=Image(source="graphs/electron.PNG",allow_stretch=True,keep_ratio=False) #we import image
+                        gridlayout[i]=GridLayout(rows=1,cols=1) #we create a gridlayout to put the image 
+                        e_position=(position[i]+5)/10 #normalising to 0-1 in the box layout
+                        gridlayout[i].pos_hint={"center_x":e_position,"center_y":0.5} #we put the gridlayout in the position we want
+                        gridlayout[i].size_hint_x=0.07/4
+                        gridlayout[i].size_hint_y=0.25/4 #size of gridlayout
+                        gridlayout[i].add_widget(e_graphs) #add the image to the gridLayout
+                        self.measure_layout.add_widget(gridlayout[i]) #we add the grid layout to the float layout
+
+
+
 
 
                      
